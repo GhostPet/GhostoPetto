43880fe5eb02a58146318a05855a39d4eec4c184
Ali Tiryaki <ali35tiryaki@gmail.com>
2022-04-15 04:23:07 +0300

    Revert "Revert "Work in progress...""
    This reverts commit 66c5e0d07d0ee885a01a8746b2a95eb818c1066a.

---
+++ MangaDownloader/WebsitePacks/v1_0/Manmantai.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MangaDownloader.app.Classes;
+using MangaDownloader.WebsitePacks.Default;
+using OpenQA.Selenium;
+
+namespace MangaDownloader.WebsitePacks.v1_0
+{
+    internal class Manmantai : IWebsite
+    {
+        string Name = "Manmantai";
+        string[] Url = { "https://www.manmantai.com/" };
+
+        string IWebsite.Name => Name;
+        string[] IWebsite.Url => Url;
+
+        public List<List<string>> GetMangas(Driver driver)
+        {
+            var browser = driver.driver;
+            browser.Navigate().GoToUrl(Url + "list/");
+            string Last = browser.FindElement(By.CssSelector("#w1 > div > ul > li.last > a")).GetAttribute("href");
+
+            List < List<string> > mangas = new List<List<string>>();
+            int Counter = 0;
+            while (browser.Url != Last)
+            {
+                browser.Navigate().GoToUrl(Url + "list_" + ++Counter + "/");
+                foreach (var element in browser.FindElements(By.ClassName("item-lg")))
+                {
+                    string MangaName = element.FindElement(By.CssSelector("p > a")).Text;
+                    string MangaUrl = element.FindElement(By.CssSelector("p > a")).GetAttribute("href");
+                    mangas.Add(new List<string> { MangaName, MangaUrl });
+                }
+            }
+            return mangas;
+        }
+
+        public List<List<string>> GetEpisodes(Driver driver, string DetailslUrl)
+        {
+            var browser = driver.driver;
+            List<List<string>> episodes = new List<List<string>>();
+            foreach (var element in browser.FindElements(By.CssSelector("#chapter-list-4 > li")))
+            {
+                string EpisodeName = element.FindElement(By.CssSelector("a > span")).Text;
+                string EpisodeUrl = element.FindElement(By.CssSelector("a")).GetAttribute("href");
+                episodes.Add(new List<string> { EpisodeName, EpisodeUrl });
+            }
+            return episodes;
+        }
+
+        public bool Download(Driver driver, string DownloadPath, string EpisodeName, string ReaderUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MangaDownloader/app/MainUi.cs
@@ -0,0 +1,90 @@
+﻿using MangaDownloader.app.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MangaDownloader.WebsitePacks.Default;
+
+namespace MangaDownloader.app
+{
+    public partial class MainUi : Form
+    {
+        Driver general;
+        Driver downloader;
+        public MainUi()
+        {
+            InitializeComponent();
+        }
+
+        private void MainUi_Load(object sender, EventArgs e)
+        {
+            // Set a Loding... text.
+            comboBox1.Text = "Loading";
+
+            // Load default theme or the selected theme from settings.
+            if (Properties.Settings.Default.Theme == "Dark") makedark();
+            else makelight();
+
+            // Prepare the selenium web browser (There's 2, 1 for search, 1 for download.)
+            this.general = new Driver(true);
+            this.downloader = new Driver(true);
+
+            // Load website packs
+            WebsitesList WebsitesList = new WebsitesList();
+            foreach (IWebsite Website in WebsitesList.Websites)
+            {
+                comboBox1.Items.Add(Website.Name);
+            }
+        }
+
+        private void aboutGhostoPettoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox newMDIChild = new AboutBox();
+            newMDIChild.ShowDialog();
+        }
+
+        private void lightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            makelight();
+        }
+
+        private void darkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            makedark();
+        }
+
+        private void makedark()
+        {
+            lightToolStripMenuItem.Checked = false;
+            darkToolStripMenuItem.Checked = true;
+            this.BackColor = SystemColors.ControlDark;
+            MenuToolbar.BackColor = SystemColors.ControlDarkDark;
+            dataGridView1.BackgroundColor = SystemColors.ControlDark;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = SystemColors.ControlDarkDark;
+            dataGridView1.DefaultCellStyle.BackColor = SystemColors.ControlDark;
+            statusStrip1.BackColor = SystemColors.ControlDark;
+            Properties.Settings.Default.Theme = "Dark";
+            Properties.Settings.Default.Save();
+        }
+
+        private void makelight()
+        {
+            lightToolStripMenuItem.Checked = true;
+            darkToolStripMenuItem.Checked = false;
+            this.BackColor = SystemColors.Control;
+            MenuToolbar.BackColor = SystemColors.Control;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = SystemColors.Control;
+            dataGridView1.DefaultCellStyle.BackColor = SystemColors.Window;
+            statusStrip1.BackColor = SystemColors.Control;
+            Properties.Settings.Default.Theme = "Light";
+            Properties.Settings.Default.Save();
+        }
+    }
+}
